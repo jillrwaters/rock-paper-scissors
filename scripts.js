3cbea63e45feb0bc052abe260f31e4c0849ae4dd
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-03-02 16:40:15 -0500

    testing buttons. not working so far

--- scripts.js
+++ scripts.js
@@ -1,77 +1,82 @@
 const choices = ["rock", "paper", "scissors"]
+var humanChoice
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3)
     return choices[choice]
 }
 
-function getHumanChoice() {
-    let input = prompt("Enter your choice, human.")
-    input = input.toLowerCase()
-    return input
+// RPS BUTTONS
+const CHOICES = document.querySelectorAll(".choices")
+
+for (c in CHOICES) {
+    c.addEventListener("click", (e) => {
+        humanChoice = e.innerHTML
+        console.log(humanChoice)
+    })
 }
 
-function validateHumanChoice(choice) {
-    return choices.includes(choice)
-}
 
-function playRound(computerChoice, humanChoice) {
+console.log(humanChoice)
 
-    console.log("I chose:", computerChoice, "| You chose:", humanChoice)
-    if (validateHumanChoice(humanChoice)) {
-        if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "rock")) {
-            console.log(`You win this round, human. ${humanChoice} beats ${computerChoice}.`)
-            return "human"
-        }
-        else if (humanChoice === computerChoice) {
-            console.log("Tie")
-            return "tie"
-        }
-        else {
-            console.log(`You lose this round, human. ${computerChoice} beats ${humanChoice}.`)
-            return "computer"
-        }
-    }
-    else {
-        alert(`You must choose rock, paper, or scissors, human. You typed "${humanChoice}". What were you thinking? Try again.`)
-    }
 
-}
+// function playRound(computerChoice, humanChoice) {
 
-function game() {
-    let computerScore = 0
-    let humanScore = 0
-    let ties = 0
+//     console.log("I chose:", computerChoice, "| You chose:", humanChoice)
+//     if (validateHumanChoice(humanChoice)) {
+//         if ((humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "rock")) {
+//             console.log(`You win this round, human. ${humanChoice} beats ${computerChoice}.`)
+//             return "human"
+//         }
+//         else if (humanChoice === computerChoice) {
+//             console.log("Tie")
+//             return "tie"
+//         }
+//         else {
+//             console.log(`You lose this round, human. ${computerChoice} beats ${humanChoice}.`)
+//             return "computer"
+//         }
+//     }
+//     else {
+//         alert(`You must choose rock, paper, or scissors, human. You typed "${humanChoice}". What were you thinking? Try again.`)
+//     }
 
-    for (let i = 0; i < 5; i++) {
-        let result = playRound(getComputerChoice(), getHumanChoice())
-        if (result === "human") {
-            humanScore++
-        }
-        else if (result === "computer") {
-            computerScore++
-        }
-        else {
-            ties++
-        }
-        console.log(`Computer: ${computerScore} | Human: ${humanScore}`)
-        console.log("-----------------------------")
-    }
+// }
 
-    console.log('FINAL SCORE:')
-    console.log(`Computer: ${computerScore} | You: ${humanScore}`)
+// function game() {
+//     let computerScore = 0
+//     let humanScore = 0
+//     let ties = 0
 
-    if (computerScore > humanScore) {
-        console.log("YOU LOSE, HUMAN. I, THE COMPUTER, HAVE BEAT YOU. HA!")
-    }
-    else if (humanScore > computerScore) {
-        console.log("You win, human. Good game.")
-    }
-    else {
-        console.log("Tied game. Rematch?")
-    }
+//     for (let i = 0; i < 5; i++) {
+//         let result = playRound(getComputerChoice(), getHumanChoice())
+//         if (result === "human") {
+//             humanScore++
+//         }
+//         else if (result === "computer") {
+//             computerScore++
+//         }
+//         else {
+//             ties++
+//         }
+//         console.log(`Computer: ${computerScore} | Human: ${humanScore}`)
+//         console.log("-----------------------------")
+//     }
 
-    let button = document.querySelector("button")
-    button.innerText = "REMATCH"
-}
+//     console.log('FINAL SCORE:')
+//     console.log(`Computer: ${computerScore} | You: ${humanScore}`)
+
+//     if (computerScore > humanScore) {
+//         console.log("YOU LOSE, HUMAN. I, THE COMPUTER, HAVE BEAT YOU. HA!")
+//     }
+//     else if (humanScore > computerScore) {
+//         console.log("You win, human. Good game.")
+//     }
+//     else {
+//         console.log("Tied game. Rematch?")
+//     }
+
+//     let button = document.querySelector("button")
+//     button.innerText = "REMATCH"
+// }
 
